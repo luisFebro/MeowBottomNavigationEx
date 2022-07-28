d64d7fa81337deb88df219a4e28cc9fb67ed20c4
luisFebro <mr.febro@gmail.com>
2022-07-28 09:30:32 -0300

    update magic navigation

--- app/src/main/java/com/codeinger/mymeowbottomnavigationview/MainActivity.java
+++ app/src/main/java/com/codeinger/mymeowbottomnavigationview/MainActivity.java
@@ -1,3 +1,4 @@
+// reference: https://www.youtube.com/watch?v=KbFvUK9LY9A
 package com.codeinger.mymeowbottomnavigationview;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,6 +7,11 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.codeinger.mymeowbottomnavigationview.BookmarkFragment;
+import com.codeinger.mymeowbottomnavigationview.HomeFragment;
+import com.codeinger.mymeowbottomnavigationview.ProfileFragment;
+import com.codeinger.mymeowbottomnavigationview.R;
+import com.codeinger.mymeowbottomnavigationview.SearchFragment;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 import kotlin.Unit;
@@ -21,33 +27,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bnv_Main = findViewById(R.id.bnv_Main);
-        bnv_Main.add(new MeowBottomNavigation.Model(1,R.drawable.home));
-        bnv_Main.add(new MeowBottomNavigation.Model(2,R.drawable.search));
-        bnv_Main.add(new MeowBottomNavigation.Model(3,R.drawable.bookmark));
-        bnv_Main.add(new MeowBottomNavigation.Model(4,R.drawable.person));
+        bnv_Main.add(new MeowBottomNavigation.Model(1, R.drawable.book));
+        bnv_Main.add(new MeowBottomNavigation.Model(2,R.drawable.home));
+        bnv_Main.add(new MeowBottomNavigation.Model(3,R.drawable.person));
 
-        bnv_Main.show(1,true);
+        bnv_Main.show(2,true);
         replace(new HomeFragment());
         bnv_Main.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
             @Override
             public Unit invoke(MeowBottomNavigation.Model model) {
                 switch (model.getId()){
                     case 1:
-                        replace(new HomeFragment());
-                        break;
-
-                    case 2:
-                        replace(new SearchFragment());
-                        break;
-
-                    case 3:
                         replace(new BookmarkFragment());
                         break;
-
-                    case 4:
+                    case 2:
+                        replace(new HomeFragment());
+                        break;
+                    case 3:
                         replace(new ProfileFragment());
                         break;
-
                 }
                 return null;
             }
